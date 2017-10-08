291988ada04107609ce50e1271e5a152cc2c39b0
nguyenduc1507 <nguyenduc1507@gmail.com>
2017-10-08 17:24:08 +0700

    commit 1

---
+++ Buoi1/TruongNguyenDuc_b1/bai1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+
+namespace IpProto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
+
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            Console.WriteLine("My IP Address is :" + myIP);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Buoi1/TruongNguyenDuc_b3/bai3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] a = new byte[4];
+            for (int i = 0; i < a.Length; i++)
+                a[i] = 10;
+            IPAddress ip = new IPAddress(a);
+            Console.WriteLine("IP: {0}", ip);
+            Console.WriteLine("Any: {0}", IPAddress.Any);
+            Console.WriteLine("Broadcast: {0}", IPAddress.Broadcast);
+            Console.WriteLine("Loopback: {0}", IPAddress.Loopback);
+
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("123.123.12.13"), 12345);
+            Console.WriteLine("IpEndpoint: {0}", ipe);
+            Console.WriteLine("IP: {0}", ipe.Address);
+            Console.WriteLine("IP: {0}", ip.AddressFamily);
+
+            IPHostEntry iphe = Dns.GetHostEntry("www.google.com");
+            Console.WriteLine("IP: {0}", iphe);
+            Console.ReadKey();
+        }
+    }
+}
